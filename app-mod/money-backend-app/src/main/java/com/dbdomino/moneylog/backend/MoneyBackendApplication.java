6a60e47c7e0b68e2e34c74df28ca8c9276d4cd27
dbdomino <dbdomino@naver.com>
2026-07-17 13:32:46 +0900

    main 소스 작성

--- app-mod/money-backend-app/src/main/java/com/dbdomino/moneylog/backend/MoneyBackendApplication.java
+++ app-mod/money-backend-app/src/main/java/com/dbdomino/moneylog/backend/MoneyBackendApplication.java
@@ -2,12 +2,8 @@ package com.dbdomino.moneylog.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.dbdomino.moneylog")
-@EntityScan(basePackages = "com.dbdomino.moneylog")
-@EnableJpaRepositories(basePackages = "com.dbdomino.moneylog")
+@SpringBootApplication(scanBasePackages = "com.dbdomino.moneylog.backend")
 public class MoneyBackendApplication {
 
 	public static void main(String[] args) {
